e941790502d4b8785b5386902bd96883ae13ade9
Utkarsh Raj <utkarshraj102005@gmail.com>
2026-02-22 00:49:28 +0530

    added dotenv & Safety MONGO URI

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,7 +1,9 @@
 // import mongoose from 'mongoose'
-const mongoose = require("mongoose")
+const express = require('express')
+require('dotenv').config();
+const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://utkarshraj102005:iZxiCfkbhXH7VREz@cluster0.mdkmorc.mongodb.net/?appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI)
 const address_schema = new mongoose.Schema({
         hNo:String,
         street:String,
